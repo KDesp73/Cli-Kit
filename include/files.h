8b2ae2357d843bbedf6dbd040594e5073d88f993
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-09-04 15:25:01 +0300

    more functions added

--- include/files.h
+++ include/files.h
@@ -8,6 +8,7 @@ namespace Files{
     std::string readFile(const std::string& filename);
     void writeFile(const std::string& filename, const std::string& data);
     void appendFile(const std::string& filename, const std::string& data);
-
+    std::string readLineFromFile(const std::string& filename, int lineNumber);
+    void writeLineToFile(const std::string& filename, int lineNumber, const std::string& lineToWrite;
 };
 };
--- src/files/files.cpp
+++ src/files/files.cpp
@@ -50,3 +50,58 @@ void Files::appendFile(const std::string& filename, const std::string& data) {
     file << data;
     file.close();
 }
+
+// Function to read a specific line from a file
+std::string Files::readLineFromFile(const std::string& filename, int lineNumber) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("File not found or cannot be opened.");
+    }
+    
+    std::string line;
+    int currentLineNumber = 0;
+    
+    while (currentLineNumber < lineNumber && std::getline(file, line)) {
+        currentLineNumber++;
+    }
+    
+    if (currentLineNumber == lineNumber) {
+        return line;
+    } else {
+        throw std::runtime_error("Line number exceeds the number of lines in the file.");
+    }
+}
+
+// Function to write a specific line to a file
+void Files::writeLineToFile(const std::string& filename, int lineNumber, const std::string& lineToWrite) {
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        throw std::runtime_error("File not found or cannot be opened.");
+    }
+    
+    std::vector<std::string> lines;
+    std::string line;
+    
+    while (std::getline(inFile, line)) {
+        lines.push_back(line);
+    }
+    
+    inFile.close();
+    
+    if (lineNumber < 1 || lineNumber > static_cast<int>(lines.size())) {
+        throw std::runtime_error("Line number is out of range.");
+    }
+    
+    lines[lineNumber - 1] = lineToWrite; // Adjust for 0-based index
+    
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) {
+        throw std::runtime_error("Unable to open file for writing.");
+    }
+    
+    for (const std::string& updatedLine : lines) {
+        outFile << updatedLine << "\n";
+    }
+    
+    outFile.close();
+}
